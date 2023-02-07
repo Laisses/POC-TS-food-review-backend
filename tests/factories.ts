217f5f11c205527d37e8cd9acab0359bc5d9208c
Laisses <laisse@gmail.com>
2023-02-06 22:41:19 -0300

    tests/factories.ts: use faker to create mock place

--- tests/factories.ts
+++ tests/factories.ts
@@ -1,10 +1,20 @@
 import { NewPlace, Place } from "../src/protocols";
 import { connection } from "../src/database";
+import { faker } from '@faker-js/faker';
 
-export const insertPlace = async ({name, category}: NewPlace) => {
-    return connection.query(`INSERT INTO places (name, category) VALUES ($1, $2);`, [name, category]);
+export const fakerPlace = {
+    name: faker.company.name(),
+    category: faker.helpers.arrayElement(["restaurante", "bar", "pub", "café"]),
 };
 
-export const updatePlace = async ({name, category, id}: Place) => {
-    return connection.query(`UPDATE places SET name=$1, category=$2 WHERE id=$3;`, [name, category, id]);
+export const fakerRatings = {
+    rating: faker.helpers.arrayElement(["terrible", "bad", "ok", "good", "great"]),
+};
+
+export const insertPlace = async () => {
+    return connection.query(`INSERT INTO places (name, category) VALUES ($1, $2);`, [fakerPlace.name, fakerPlace.category]);
+};
+
+export const updatePlace = async ({id}) => {
+    return connection.query(`UPDATE places SET name=$1, category=$2 WHERE id=$3;`, [fakerPlace.name, fakerPlace.category, id]);
 };
