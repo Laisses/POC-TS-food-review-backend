8e75948f424e8cfa9b6a8fa149541a24839374fd
Laisses <laisse@gmail.com>
2023-02-06 22:49:04 -0300

    tests/factories.ts: remove unused variables and functions

--- tests/factories.ts
+++ tests/factories.ts
@@ -1,8 +1,8 @@
-import { NewPlace, Place } from "../src/protocols";
+import { NewPlace } from "../src/protocols";
 import { connection } from "../src/database";
 import { faker } from '@faker-js/faker';
 
-export const fakerPlace = {
+export const fakerPlace: NewPlace = {
     name: faker.company.name(),
     category: faker.helpers.arrayElement(["restaurante", "bar", "pub", "café"]),
 };
@@ -13,8 +13,4 @@ export const fakerRatings = {
 
 export const insertPlace = async () => {
     return connection.query(`INSERT INTO places (name, category) VALUES ($1, $2);`, [fakerPlace.name, fakerPlace.category]);
-};
-
-export const updatePlace = async ({id}) => {
-    return connection.query(`UPDATE places SET name=$1, category=$2 WHERE id=$3;`, [fakerPlace.name, fakerPlace.category, id]);
 };
